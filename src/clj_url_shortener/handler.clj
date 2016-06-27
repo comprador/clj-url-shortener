0ca76a77d0dbfeae3a6ea4c514e4e3c997a4d6a7
Rajesh Shah <raj@12nines.com>
2016-06-27 22:10:54 +0100

    we can shorten and expand urls

--- src/clj_url_shortener/handler.clj
+++ src/clj_url_shortener/handler.clj
@@ -1,23 +1,49 @@
 (ns clj-url-shortener.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.util.response :as ringutil]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
   (:import 
     org.apache.commons.validator.routines.UrlValidator))
 
 (def validator (UrlValidator. (into-array ["http" "https"]) (. UrlValidator ALLOW_LOCAL_URLS)))
 
+(def urls (atom {}))
+
+(def id-generator
+  (atom 0))
+
+(defn next-id []
+  (swap! id-generator inc))
+
+(defn store [token url]
+  (swap! urls assoc token url)
+  (spit "target/out.data" @urls))
+
+(defn load-url-from-file
+  []
+  (try
+    (let [loaded-urls (read-string (slurp "target/out.data"))]
+      (reset! urls loaded-urls))
+       (catch Exception e
+         (println e))))
+
 (defn shorten [params]
   (let [url (:url params)]
     (if (.isValid validator url)
-      (str "valid url: " url)
+      (do
+        (store (str (next-id)) url)
+        (println @urls)
+        (str "valid url: " url))
       (str "invalid url: " url)
       )
     )
   )
 
-(defn expand [code]
-  (str "expanding " code)
+(defn expand [token]
+  (println "expanding " token)
+  (ringutil/redirect (@urls token))
+
   )
 
 
@@ -28,4 +54,7 @@
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (do
+    (println "Starting...")
+    (load-url-from-file)
+    (wrap-defaults app-routes site-defaults)))
